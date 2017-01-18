31f1876e4067c31c18f96d468c28e464821ea122
mohdmalik02 <mohdmalik02@gmail.com>
2017-01-18 18:42:09 +0100

    Added more languages

--- verbStem.sh
+++ verbStem.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-#Uses verbmaps to get the future proche stem for the verb
-curl verbmaps.com/en/verb/fr/$1 | perl -pe 's/\s//gs' | perl -pe 's/.*>([^><]+)<\/span><\/div><divclass="transform">.*>Add'+$2+'.*/$1\n/gi' > output.txt
+#This is a sript that gets the verb stem for the below stated 4 languages for several tenses. It fetches the data from verbmaps.com
+#Multi-lingual support (French, Spanish, Italian, Portugese)
+#To run it:
+#$ sh verbStem.sh verb tense language
+#For example:
+#$ sh verbStem.sh avoir future french
+language=$(echo $3 | perl -pe "s/spanish/es/gi" | perl -pe "s/french/fr/gi" | perl -pe "s/portugese/pt/gi" | perl -pe "s/italian/it/gi")
+curl verbmaps.com/en/verb/$language/$1 | perl -pe 's/\s//gs' | perl -pe 's/.*>([^><]+)<\/span><\/div><divclass="transform">.*>Add'+$2+'.*/$1\n/gi' > output.txt
 echo "================================================================================"
+#If the output is longer than 20 characters, we remove it since it returned nothing.
 cat output.txt | perl -pe 's/.{20,}/Error 404\n/g'
