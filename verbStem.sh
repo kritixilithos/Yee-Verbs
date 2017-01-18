f1f448f55aa289bc72b7528a5dd1ff4213afdcc6
mohdmalik02 <mohdmalik02@gmail.com>
2017-01-18 14:29:15 +0100

    modified to support other tenses

--- verbStem.sh
+++ verbStem.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Uses verbmaps to get the future proche stem for the verb
-curl verbmaps.com/en/verb/fr/$1 | perl -pe 's/\s//gs' | perl -pe 's/.*>([^><]+)<\/span><\/div><divclass="transform">.*>Addfuture.*/$1\n/gi' > output.txt
+curl verbmaps.com/en/verb/fr/$1 | perl -pe 's/\s//gs' | perl -pe 's/.*>([^><]+)<\/span><\/div><divclass="transform">.*>Add'+$2+'.*/$1\n/gi' > output.txt
 echo "================================================================================"
 cat output.txt | perl -pe 's/.{20,}/Error 404\n/g'
